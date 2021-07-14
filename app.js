ae341a454048091ceb5e2facb8fdcbaf16fc5378
mpdiaz <mpdiaz@amazon.com>
2021-07-14 08:29:24 -0500

    wip

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors')
 
 // Constants
 const PORT = 80;
@@ -8,8 +9,30 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Hello Maria');
+});
+
+app.get('/healthCheck', (req, res) => {
+    res.send("I'm healthy guys :)");
+});
+
+app.get('/journey', (req, res) => {
+    res.send("This is a journey");
+});
+
+app.post('/journey', (req, res) => {
+    res.send("This is a journey");
+});
+
+app.put('/journey', (req, res) => {
+    res.send("This is a journey");
+});
+
+app.delete('/journey', (req, res) => {
+    res.send("This is a journey");
 });
 
 app.listen(PORT, HOST);
